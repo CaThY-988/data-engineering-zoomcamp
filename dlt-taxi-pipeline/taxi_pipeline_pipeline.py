076657e917594c19696374416809fc4f93194b23
Catherine Frost <catherinefrost@Catherines-MacBook-Air.local>
2026-02-28 12:44:16 +0000

    add basic dtl pipeline

---
+++ dlt-taxi-pipeline/taxi_pipeline_pipeline.py
@@ -0,0 +1,54 @@
+"""Template for building a `dlt` pipeline to ingest data from a REST API."""
+
+import dlt
+from dlt.sources.rest_api import rest_api_resources
+from dlt.sources.rest_api.typing import RESTAPIConfig
+
+
+# if no argument is provided, `access_token` is read from `.dlt/secrets.toml`
+@dlt.source
+def taxi_pipeline_rest_api_source(access_token: str = dlt.secrets.value):
+    """Define dlt resources from REST API endpoints."""
+    config: RESTAPIConfig = {
+        "client": {
+            "base_url": "https://us-central1-dlthub-analytics.cloudfunctions.net/data_engineering_zoomcamp_api",
+        },
+        "resource_defaults": {
+            "write_disposition": "replace"
+        },
+        "resources": [
+            {
+                "name": "trips",
+                "endpoint": {
+                    "path": "",
+                    "params": {"page_size": 1000},
+                    "paginator": {
+                        "type": "page_number",
+                        "page_param": "page",
+                        "base_page": 1,
+                        "total_path": None,
+                        "stop_after_empty_page": True,
+                    }
+                }
+            }
+        ]
+    }
+
+    yield from rest_api_resources(config)
+
+
+pipeline = dlt.pipeline(
+    pipeline_name='taxi_pipeline_pipeline',
+    destination='duckdb',
+    # `refresh="drop_sources"` ensures the data and the state is cleaned
+    # on each `pipeline.run()`; remove the argument once you have a
+    # working pipeline.
+    refresh="drop_sources",
+    # show basic progress of resources extracted, normalized files and load-jobs on stdout
+    progress="log",
+)
+
+
+if __name__ == "__main__":
+    load_info = pipeline.run(taxi_pipeline_rest_api_source())
+    print(load_info)  # noqa: T201
